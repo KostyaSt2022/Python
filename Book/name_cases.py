0c3847f300eb15ca74d9760ffe09027992c68cca
Kostya <kotekostya@gmail.com>
2023-01-15 20:48:50 +0400

    Задания из книги

---
+++ Book/name_cases.py
@@ -0,0 +1,25 @@
+# name = input('Введите имя: ')
+# print(f'Hello, {name}, would you like to learn some Python today?')
+
+
+# f_name = input('Введите имя: ')
+# l_name = input('Введите фамилию: ')
+# full = f'{f_name} {l_name}'
+# print(f'Hello, {full.upper()} / {full.lower()} / {full.title()}')
+
+# citata = 'Albert Einstein once said, "A person who never made a mistake never tried anything new"'
+# print(citata)
+
+# famous_person = 'Albert Einstein'
+# message = '"A person who never made a mistake never tried anything new"'
+# print(f'{famous_person} {message}')
+
+
+# name = '\tKostya Stepanov\n'
+# print(name)
+# name = name.rstrip() #удаление пробелов справа
+# print(name)
+# name = name.lstrip() #удаление пробелов слева
+# print(name)
+# name = name.strip() #удаление пробелов с обеих сторон
+# print(name)
